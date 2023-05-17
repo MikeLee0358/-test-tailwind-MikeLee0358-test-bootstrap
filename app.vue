7daf52f8b96b20eddbf65c6bd20d2bbf805b6eb8
mike <cspkno005@gmail.com>
2023-05-17 15:19:08 +0800

    nothing

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1 class="fs-2 fw-bold underline">
+  <h1 class="fs-2  fw-bold underline">
     Hello world!
   </h1>
 </template>
 
-<style >
-.underline {
-  text-decoration-line: underline;
-}
+<style lang="sass">
+.underline 
+  text-decoration-line: underline
+
 </style>
