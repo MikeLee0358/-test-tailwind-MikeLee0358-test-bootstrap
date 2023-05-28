cd1e677391e8e2f75cf76d07e6b3b136bfe6c2dc
mike <cspkno005@gmail.com>
2023-05-29 01:34:30 +0800

    新增新增postCss和preflight.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["bootstrap/dist/css/bootstrap.min.css"],
-
+    css: ["bootstrap/dist/css/bootstrap.min.css", "@/assets/css/preflight.css"],
+    modules: [
+        // Simple usage
+        'nuxt-purgecss',
+        // With options
+    ],
+    purgecss: {
+        enabled: true, // Always enable purgecss
+        safelist: ['my-class'], // Add my-class token to the safelist (e.g. .my-class)
+    },
+    experimental: {
+        inlineSSRStyles: false
+    }
 })
